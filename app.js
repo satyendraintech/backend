3b1e59caa870b215233b2dae1c5f2bdcea173d84
Satyendra Singh <satyendrasingh@titan.co.in>
2025-01-24 12:39:49 +0530

    done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB = require("./src/config/db");
 
 const app = express();
 
@@ -29,15 +29,15 @@ app.use(
 app.get("/", (req, res) => res.send("API is running"));
 
 // Routes
-const authRoutes = require("./routes/authRoutes");
-const protectedRoutes = require("./routes/protectedRoutes");
-const postRoutes = require("./routes/postRoutes");
-const commentRoutes = require("./routes/commentRoutes");
-const contactRoutes = require("./routes/contactRoutes");
-const experienceRoutes = require("./routes/experienceRoutes");
-const smsRoutes = require("./routes/smsRoutes"); // Use the new smsRoutes
-const workExperience = require("./routes/workRoutes");
-const careerJourney = require("./routes/careerJourneyRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+const protectedRoutes = require("./src/routes/protectedRoutes");
+const postRoutes = require("./src/routes/postRoutes");
+const commentRoutes = require("./src/routes/commentRoutes");
+const contactRoutes = require("./src/routes/contactRoutes");
+const experienceRoutes = require("./src/routes/experienceRoutes");
+const smsRoutes = require("./src/routes/smsRoutes"); // Use the new smsRoutes
+const workExperience = require("./src/routes/workRoutes");
+const careerJourney = require("./src/routes/careerJourneyRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api", protectedRoutes);
@@ -48,5 +48,11 @@ app.use("/api/experience", experienceRoutes);
 app.use("/api/sms", smsRoutes);
 app.use("/api/work", workExperience);
 app.use("/api/career-journey", careerJourney);
+
+// Server configuration
+const config = require("./src/config/config");
+const PORT = config.port;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./src/app");
-const config = require("./src/config/config");
-
-const PORT = config.port;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
